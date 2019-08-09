4f8c4c5f96e3350281424b896507986f0e0640e7
Vladimir <setyaev_v@pgstudio.io>
2019-08-09 17:55:40 +0300

    StatusCode Complete

---
+++ NetOperationTest/StatusCodeTest.cs
@@ -0,0 +1,65 @@
+using System;
+using NetworkOperation;
+using Xunit;
+
+namespace NetOperationTest
+{
+    public enum FooCodes
+    {
+        A,
+        B,
+        C
+    }
+    
+    public enum BarCodes
+    {
+        A,
+        B,
+        C
+    }
+    
+    public class StatusCodeTest
+    {
+        public StatusCodeTest()
+        {
+            StatusCode.Register(typeof(FooCodes), typeof(BarCodes));
+        }
+        
+        [Fact]
+        public void should_be_converted_from_enum()
+        {
+            StatusCode b = FooCodes.A;
+            Assert.Equal(0000_0001u,b.Code);
+            
+            StatusCode a = BarCodes.A;
+            Assert.Equal(0000_0002u,a.Code);
+        }
+        
+        
+        [Fact]
+        public void check_operators()
+        {
+            StatusCode a = BarCodes.A;
+            StatusCode b = FooCodes.A;
+            StatusCode c = BarCodes.A;
+            
+            Assert.NotEqual(a,b);
+            Assert.Equal(a,c);
+            
+            Assert.True(b == FooCodes.A,"b == FooCodes.A");
+            Assert.True(b >= FooCodes.A,"b >= FooCodes.A");
+            Assert.True(b <= BarCodes.A,"b <= BarCodes.A");
+            Assert.True(b < BarCodes.A,"b < BarCodes.A");
+        }
+        
+        [Fact]
+        public void should_be_throw()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                StatusCode a = BarCodes.A;
+                StatusCode.Register(typeof(FooCodes), typeof(BarCodes));
+            });
+        }
+    }
+}

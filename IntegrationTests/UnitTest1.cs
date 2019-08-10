7ff34c10bdbf75781b51e3526eed223b262598b0
w0lf163 <wlf6903@gmail.com>
2019-08-10 17:26:47 +0300

    preparation for integration tests

---
+++ IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class ServerClientInteractionTest
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
